d59cd5126121e7b23e1c95dff7c0264fce7f3e80
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-08-03 23:25:57 +0530

    fix: RNG Class exportation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export * from "./interfaces/IRNG";
+import { RNG as RngBrowser } from "./rng.browser";
+import { RNG as RngNode } from "./rng.node";
+
+let RNGClass;
+
+if (typeof window !== "undefined" && window.crypto) {
+    RNGClass = RngBrowser;
+} else {
+    RNGClass = RngNode;
+}
+
+export const RNG = RNGClass;
